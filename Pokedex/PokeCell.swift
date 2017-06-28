12cedcf0f5d247107f93ae95ae00e33d79618666
Antonio Villavicencio <jantoniovc@me.com>
2017-06-28 15:45:34 -0500

    Protocol for CollectionView implemented, Test Cells

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,31 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Antonio Villavicencio on 6/28/17.
+//  Copyright © 2017 Antonio Villavicencio. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        thumbImg.image = UIImage(named: "\(pokemon.pokedexID)")
+        nameLbl.text = pokemon.name.capitalized
+    }
+    
+}
--- Pokedex/Pokemon.swift
+++ Pokedex/Pokemon.swift
@@ -11,17 +11,17 @@ import Foundation
 class Pokemon {
     
     private var _name: String!
-    private var _pokedexID: String!
+    private var _pokedexID: Int!
     
     var name: String {
         return _name
     }
     
-    var pokedexID: String {
+    var pokedexID: Int {
         return _pokedexID
     }
     
-    init(name: String, pokedexID: String) {
+    init(name: String, pokedexID: Int) {
         self._name = name
         self._pokedexID = pokedexID
     }
